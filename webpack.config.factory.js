97bacdf985d9b591ebd1852d0dd1d07112922bcf
Daniel Perez Alvarez <unindented@gmail.com>
2016-03-22 08:41:14 +0000

    Optimize module and chunk IDs

--- webpack.config.factory.js
+++ webpack.config.factory.js
@@ -154,6 +154,7 @@ module.exports = function (options) {
         }
       })
     ].concat(optimize ? [
+      new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.UglifyJsPlugin({
         compressor: {
           screw_ie8: true,
