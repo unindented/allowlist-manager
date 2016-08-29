db675d3359aa585573631ae846e2345a2b050cc1
Daniel Perez Alvarez <unindented@gmail.com>
2016-08-29 16:48:50 +0100

    Allow IPs and "localhost" in the whitelist

--- src/utils/glob/src/index.js
+++ src/utils/glob/src/index.js
@@ -1,13 +1,19 @@
 import constant from 'lodash/constant'
 import convert from './convert'
 
-const validDomain = /^.+\.[^.]+$/
+const ipv4 = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+const ipv6 = /^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/
+const localhost = /^localhost$/
+const domain = /^.+\.[^.]+$/
 const simpleDomain = /^[^.*]+\.[^.]+$/
 const noop = constant(false)
 
 export function compile (glob) {
   // If not a valid glob, ignore it.
-  if (!validDomain.test(glob)) {
+  if (!ipv4.test(glob) &&
+      !ipv6.test(glob) &&
+      !localhost.test(glob) &&
+      !domain.test(glob)) {
     return noop
   }
 
